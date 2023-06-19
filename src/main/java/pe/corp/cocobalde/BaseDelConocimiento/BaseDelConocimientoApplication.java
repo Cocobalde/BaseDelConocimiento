475b22b3e99daa601c9fa59222faf4f6f5d57a60
Cocobalde <brunofabianjesus400@gmail.com>
2023-06-19 14:24:11 -0500

    Se agregó el proyecto siuuuuu

---
+++ src/main/java/pe/corp/cocobalde/BaseDelConocimiento/BaseDelConocimientoApplication.java
@@ -0,0 +1,13 @@
+package pe.corp.cocobalde.BaseDelConocimiento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseDelConocimientoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseDelConocimientoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/corp/cocobalde/BaseDelConocimiento/BaseDelConocimientoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.corp.cocobalde.BaseDelConocimiento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseDelConocimientoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
